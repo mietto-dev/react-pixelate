dd97d0c2aca063cb914277a58c19f19ea384764e
Felipe Mietto <mietto.ti@gmail.com>
2024-01-17 14:50:06 -0300

    refactor: reorganize cleanup

--- src/pixelate/pixelate.tsx
+++ src/pixelate/pixelate.tsx
@@ -1,19 +1,54 @@
 import React from "react";
 import { PixelateProps } from "./pixelate.types";
 
+/**
+ * Renders a pixelated version of an image using HTML Canvas
+ */
 const Pixelate: React.FC<PixelateProps> = ({
   src,
-  width = "100%",
-  height = "100%",
-  pixelSize = 8,
+  width,
+  height,
+  pixelSize,
   centered = true,
   fillTransparencyColor,
-}) => {
+}: PixelateProps) => {
   const [imgLoaded, setImgLoaded] = React.useState(false);
   const canvas = React.useRef(document.createElement("canvas"));
   const img = React.useRef(document.createElement("img"));
 
-  const process = () => {
+  React.useEffect(() => {
+    loadImage();
+  }, []);
+
+  React.useEffect(() => {
+    if (imgLoaded) {
+      const ctx = canvas.current.getContext("2d");
+
+      // width and height override
+      img.current.width = width ? width : img.current.width;
+      canvas.current.width = width ? width : img.current.width;
+      img.current.height = height ? height : img.current.height;
+      canvas.current.height = height ? height : img.current.height;
+
+      if (!width) {
+        canvas.current.style.width = "100%";
+        img.current.style.width = "100%";
+      }
+      if (!height) {
+        canvas.current.style.height = "100%";
+        img.current.style.height = "100%";
+      }
+
+      if (ctx) {
+        // we paint the image into the canvas
+        // this is needed to get RGBA info out of each pixel
+        ctx.drawImage(img.current, 0, 0, img.current.width, img.current.height);
+        paintPixels(ctx);
+      }
+    }
+  }, [imgLoaded]);
+
+  const loadImage = () => {
     // create img that will be later painted into the canvas
     let imgLoad = new Image();
     imgLoad.crossOrigin = "anonymous";
@@ -26,32 +61,25 @@ const Pixelate: React.FC<PixelateProps> = ({
     };
   };
 
-  const paintPixels = (ctx) => {
-    let pixelSizeN = 4;
+  // TODO add effect to recompute pixel size in case image size changes
+  const paintPixels = (ctx: CanvasRenderingContext2D) => {
+    const calcPixel =
+      pixelSize ||
+      getRelativePixelSize(canvas.current.parentElement.clientWidth);
 
-    try {
-      pixelSizeN = parseInt(String(pixelSize));
-    } catch (err) {
-      console.error("pixelSize must be a number!");
-    }
-
-    if (typeof width === "string" && width.endsWith("%")) {
-      pixelSizeN = Math.round(canvas.current.parentElement.clientWidth / 10);
-    }
-
-    if (!isNaN(pixelSizeN) && pixelSizeN > 0) {
-      for (let x = 0; x < img.current.width + pixelSizeN; x += pixelSizeN) {
-        for (let y = 0; y < img.current.height + pixelSizeN; y += pixelSizeN) {
+    if (!isNaN(calcPixel) && calcPixel > 0) {
+      for (let x = 0; x < img.current.width + calcPixel; x += calcPixel) {
+        for (let y = 0; y < img.current.height + calcPixel; y += calcPixel) {
           let xColorPick = x;
           let yColorPick = y;
 
           if (x >= img.current.width) {
             xColorPick =
-              x - (pixelSizeN - (img.current.width % pixelSizeN) / 2) + 1;
+              x - (calcPixel - (img.current.width % calcPixel) / 2) + 1;
           }
           if (y >= img.current.height) {
             yColorPick =
-              y - (pixelSizeN - (img.current.height % pixelSizeN) / 2) + 1;
+              y - (calcPixel - (img.current.height % calcPixel) / 2) + 1;
           }
 
           const rgba = ctx.getImageData(xColorPick, yColorPick, 1, 1).data;
@@ -66,68 +94,24 @@ const Pixelate: React.FC<PixelateProps> = ({
 
           if (centered) {
             ctx.fillRect(
-              // parseInt(
-              x - (pixelSizeN - (img.current.width % pixelSizeN) / 2),
-              // ),
-              // parseInt(
-              y - (pixelSizeN - (img.current.height % pixelSizeN) / 2),
-              // ),
-              pixelSizeN,
-              pixelSizeN
+              x - (calcPixel - (img.current.width % calcPixel) / 2),
+              y - (calcPixel - (img.current.height % calcPixel) / 2),
+              calcPixel,
+              calcPixel
             );
           } else {
-            ctx.fillRect(x, y, pixelSizeN, pixelSizeN);
+            ctx.fillRect(x, y, calcPixel, calcPixel);
           }
         }
       }
     }
   };
 
-  React.useEffect(() => {
-    if (imgLoaded) {
-      const canvasX = canvas.current;
-      const ctx = canvasX.getContext("2d");
+  const getRelativePixelSize = (width: number) => {
+    return width >= 600 ? 10 : width >= 400 ? 15 : width >= 200 ? 20 : 25;
+  };
 
-      img.current.width = typeof width === "number" ? width : img.current.width;
-      img.current.height =
-        typeof height === "number" ? height : img.current.height;
-
-      canvas.current.width = img.current.width;
-      canvas.current.height = img.current.height;
-
-      if (typeof width === "string" && width.endsWith("%")) {
-        canvas.current.style.width = width;
-        img.current.style.width = width;
-      }
-      if (typeof height === "string" && height.endsWith("%")) {
-        canvas.current.style.height = height;
-        img.current.style.height = height;
-      }
-
-      if (ctx) {
-        // we paint the image into the canvas
-        // this is needed to get RGBA info out of each pixel
-        ctx.drawImage(img.current, 0, 0, img.current.width, img.current.height);
-        paintPixels(ctx);
-        img.current = document.createElement("img");
-      }
-    }
-  }, [imgLoaded]);
-
-  React.useEffect(() => {
-    process();
-  }, []);
-
-  return (
-    <>
-      <div
-        className="img-container"
-        style={{ width: "200px", height: "170px" }}
-      >
-        <canvas ref={canvas} />
-      </div>
-    </>
-  );
+  return <canvas ref={canvas} />;
 };
 
 export { Pixelate };
--- src/pixelate/pixelate.types.ts
+++ src/pixelate/pixelate.types.ts
@@ -1,8 +1,16 @@
+/**
+ * Pixelate component props
+ * @property {string} src - The imported image / path
+ * @property {number} width - Image specific width in pixels (defaults to 100% of parent container)
+ * @property {number} height - Image specific height in pixels (defaults to 100% of parent container)
+ * @property {number} pixelSize - Pixel size in the new pixelated image (defaults to 10 - 25 range, depending on image width)
+ * @interface
+ */
 export interface PixelateProps {
   src: string;
-  width?: string | number;
-  height?: string | number;
-  pixelSize?: string | number;
+  width?: number;
+  height?: number;
+  pixelSize?: number;
   centered?: boolean;
   fillTransparencyColor?: string;
 }
